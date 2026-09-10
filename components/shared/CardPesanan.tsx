fe578ab8d58bcc82f87adbed051d36e283538f88
nurm04 <nurm0892709@gmail.com>
2026-09-10 17:41:21 +0700

    a

--- components/shared/CardPesanan.tsx
+++ components/shared/CardPesanan.tsx
@@ -128,7 +128,7 @@ export default function CardPesanan({ pesanan }: Props) {
               
               <div className="relative w-16 h-16 overflow-hidden border shrink-0 bg-base-200 rounded-xl border-base-300">
                 <Image
-                  src="https://admin.bikincetak.co.id/storage/img_web/logobikincetak.png"
+                  src={item.gambar_url || "https://admin.bikincetak.co.id/storage/img_web/logobikincetak.png"}
                   alt={cleanProductName || "Produk"}
                   fill
                   className="object-cover"
